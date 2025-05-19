a7b6c06dcd61b35a670c08a650d834e9fc0d5a97
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2025-05-19 22:46:18 +0300

    add front

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,3 +1,53 @@
-function App() {}
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, seNewTask] = useState("");
+
+  useEffect(() => {
+    fetch("api/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  });
+
+  const addTask = () => {
+    fetch("api/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text: newTask }),
+    })
+      .then((res) => res.json())
+      .then((data) => setTasks([...tasks, data]));
+    setNewTask("");
+  };
+
+  const deleteTask = (id) => {
+    fetch("api/tasks/${id}", {
+      method: "DELETE",
+    }).then(() => setTasks(tasks.filter((task) => task.id !== id)));
+  };
+
+  return (
+    <div className="App">
+      <h1>TODOOSHKA</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="New task"
+      />
+      <button onClick={addTask}>Add task</button>
+      <ul>
+        {tasks.map((task) => {
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => deleteTask(task.id)}>Delete task</button>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
